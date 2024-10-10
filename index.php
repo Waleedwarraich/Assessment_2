a8f051a8edf7eddd4f0993ce095924f16dfa73ea
SaifullahAsif <saifullahasif9000@gmail.com>
2024-10-10 21:04:41 +0500

    Images set successfully

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
     </div>
     <h1 id="mainHeadingHome">Pets Victoria</h1>
     <h2 id="welcomeHeading">WELCOME TO PET ADOPTION</h2>
-    <img src="/images/main.jpg" alt="Apostles Image" class="circle-img">
+    <img src="./images/main.jpg" alt="Apostles Image" class="circle-img">
 </main>
 <?php include "includes/footer.inc"?>
